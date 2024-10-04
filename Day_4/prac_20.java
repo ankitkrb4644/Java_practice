0f6e21cae4aa0cf053b822505f86ea21ca87e621
Ankit <ankitkrb4644@gmail.com>
2024-10-05 02:45:38 +0530

    update

---
+++ Day_4/prac_20.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+public class prac_20 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter temperature in Fahrenheit:");
+        double fahrenheit = Double.parseDouble(br.readLine());
+        
+        double celsius = (fahrenheit - 32) * 5 / 9;
+        
+        System.out.println("Temperature in Celsius: " + celsius);
+    }
+}
