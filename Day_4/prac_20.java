c55cd79a3f96bf381bb7f002250ce80efaa4d3d4
Ankit <ankitkrb4644@gmail.com>
2024-10-05 04:03:06 +0530

    update

--- Day_4/prac_20.java
+++ Day_4/prac_20.java
@@ -1,7 +1,7 @@
 import java.io.*;
 
 public class prac_20 {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args){
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Enter temperature in Fahrenheit:");
         double fahrenheit = Double.parseDouble(br.readLine());
