e525dac9cf5f8d6cc54b85ea290ad6ee69e02277
Ankit <ankitkrb4644@gmail.com>
2024-10-09 21:08:15 +0530

    update

--- Day_5/Prac_1.java
+++ Day_5/Prac_1.java
@@ -1,7 +1,19 @@
-package Day_5;
-
-public class Prac_1 {
-    public static void main(String[] args) {
-        
+import java.io.*;
+class Prac_1{
+public static void changeCase(String s){
+    // code here
+    String result = s.substring(0, 1).toUpperCase() + s.substring(1);
+    System.out.println(result);
+    String s1 = s.toUpperCase();
+    System.out.println(s1);
+}
+    public static void main(String args[])throws IOException{
+        BufferedReader read = new BufferedReader( new InputStreamReader(System.in));
+        int n = Integer.parseInt(read.readLine());
+        while( n-- > 0){
+            String s = read.readLine().trim();
+            
+            changeCase(s);
+        }
     }
 }
