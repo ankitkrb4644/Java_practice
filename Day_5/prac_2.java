a5da98995d855a9459cfbe9f4f5dbfab9b2708c2
Ankit <ankitkrb4644@gmail.com>
2024-10-10 22:51:37 +0530

    update

--- Day_5/prac_2.java
+++ Day_5/prac_2.java
@@ -1,10 +1,28 @@
-package Day_5;
+
 import java.io.*;
+
 public class prac_2 {
-    BufferedReader bc = new BufferedReader ( new InputStreamReader(System.in));
-    int n = Integer.parse.Int(bc.readLine());
-    while(n-- > o){
-        char c1 = bc.readLine().trim().charAt(0);
-        char c2 = bc.readLine().trim().charAt(0);
+    public static void main(String[] args)throws IOException {
+        BufferedReader bc = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(bc.readLine());
+        while (n-- > 0) {
+            char c1 = bc.readLine().trim().charAt(0);
+            char c2 = bc.readLine().trim().charAt(0);
+
+            alphabets(c1, c2);
+        }
+    }
+public static void alphabets(char c1 , char c2) {
+        if(c1 <= c2){
+            for(char ch=c1; ch<=c2; ch++){
+                System.out.print(ch + " ");
+            }
+        }
+        else {
+            for(char ch = c1; ch >= c2; ch--){
+                System.out.print(ch + " ");
+            }
+        }
+        System.out.println();
     }
 }
