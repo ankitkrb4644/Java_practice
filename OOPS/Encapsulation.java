91ebe83e48b54269a1c2b282be82e8e79c0b243b
Ankit <ankitkrb4644@gmail.com>
2025-02-16 11:15:12 +0530

    update

--- OOPS/Encapsulation.java
+++ OOPS/Encapsulation.java
@@ -5,15 +5,21 @@ class Human{
     public int getAge(){
         return age;
     }
+    public void setAge(int a){
+        age = a;
+    }
     public String getName(){
         return name;
+    }
+    public void setName(String n){
+        name = n;
     }
 }
 public class Encapsulation{
     public static void main(String[] args) {
         Human obj = new Human();
-        // obj.age = 30;
-        // obj.name = "Ankit";
+        obj.setAge(30);
+        obj.setName("Ankit");
         System.out.println(obj.getName() + ":" + obj.getAge());
     }
 }
---
+++ OOPS/source_file.java
@@ -0,0 +1,9 @@
+class A {
+        String a = "Ankit";
+    }
+public class source_file{
+    public static void main(String[] args) {
+        A obj = new A();
+        System.out.println(obj.a);
+    }
+}
